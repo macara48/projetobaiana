12d4de82dd5ef3b3974676960dee842e7d1a7cf0
Maria Clara Pires <80272737+macara48@users.noreply.github.com>
2025-11-15 15:11:35 -0300

    Update aluno_dao.py

--- projetoBaiana/dao/aluno_dao.py
+++ projetoBaiana/dao/aluno_dao.py
@@ -5,44 +5,44 @@ DAO (Data Access Object) para operações de banco de dados da tabela aluno
 from bd.database import DatabaseConnection
 from dao.nivel_dao import NivelDAO
 from model.aluno_class import Aluno
-from model.nivel_class import Nivel
+from model.parametros_class import Parametros
 
 class AlunoDAO:
     def __init__(self, db: DatabaseConnection):
-        self.db = db
-
+        self.__db = db
+    
     def salvar(self, aluno: Aluno):
-        cur = self.db.cursor()
+        cur = self.__db.cursor()
         
         # Converter boolean para integer (SQLite)
         ativoInt = 1 if aluno.ativo else 0
         
-        nivelId = aluno.nivel.nivel_id
+        nivelId = aluno.nivel.id
         
         if aluno.id is None:
             # INSERT
             cur.execute("""
-                INSERT INTO aluno (aluno_id, nome, contato, nivel, tipoConducao,
-                                 ativo)
-                VALUES (?, ?, ?, ?, ?, ?);
-            """, (aluno.aluno_id, aluno.nome, aluno.contato, aluno.nivel, aluno.tipoConducao,
-                  aluno.ativo))
+                INSERT INTO aluno (nome, contato, tipoConducao,
+                                 ativo, nivel_id)
+                VALUES (?, ?, ?, ?, ?);
+            """, (aluno.nome, aluno.contato, aluno.tipoConducao,
+                  ativoInt, nivelId))
 
-            aluno.aluno_id = cur.lastrowid
+            aluno.id = cur.lastrowid
         else:
             # UPDATE
             cur.execute("""
-                UPDATE aluno SET aluno_id = ?, nome = ?, contato = ?, nivel = ?, tipoConducao = ?,
-                               ativo = ?
+                UPDATE aluno SET nome = ?, contato = ?, tipoConducao = ?, ativo = ?,
+                               nivel_id = ?
                 WHERE id = ?;
-            """, (aluno.aluno_id, aluno.nome, aluno.contato, aluno.nivel, aluno.tipoConducao,
-                  aluno.ativo))
+            """, (aluno.nome, aluno.contato, aluno.tipoConducao,
+                  ativoInt, nivelId))
         
-        return aluno.aluno_id
+        return aluno.id
     
     def buscarPorId(self, id: int):
-        cur = self.db.cursor()
-        cur.execute("SELECT * FROM pessoa WHERE id = ?;", (id,))
+        cur = self.__db.cursor()
+        cur.execute("SELECT * FROM aluno WHERE id = ?;", (id,))
         row = cur.fetchone()
         
         if row:
@@ -50,8 +50,8 @@ class AlunoDAO:
         return None
     
     def buscarPorNome(self, nome: str):
-        cur = self.db.cursor()
-        cur.execute("SELECT * FROM pessoa WHERE nome LIKE ?;", (f'%{nome}%',))
+        cur = self.__db.cursor()
+        cur.execute("SELECT * FROM aluno WHERE nome LIKE ?;", (f'%{nome}%',))
         rows = cur.fetchall()
         
         resultado = []
@@ -60,11 +60,11 @@ class AlunoDAO:
         return resultado
     
     def listarTodas(self, comNivel: bool = False):
-        cur = self.db.cursor()
+        cur = self.__db.cursor()
         
         if comNivel:
             cur.execute("""
-                SELECT a.*, n.corNivel as nivel_nome
+                SELECT a.*, n.nome as nivel_nome
                 FROM aluno a
                 JOIN nivel n ON a.nivel_id = n.id
                 ORDER BY a.nome;
@@ -78,9 +78,9 @@ class AlunoDAO:
             resultado.append(self.criarDeRow(row))
         return resultado
     
-    def buscarPorCategoria(self, nivel_id: int):
-        cur = self.db.cursor()
-        cur.execute("SELECT * FROM aluno WHERE nivel_id = ? ORDER BY nome;", (nivel_id,))
+    def buscarPorNivel(self, nivelId: int):
+        cur = self.__db.cursor()
+        cur.execute("SELECT * FROM aluno WHERE nivel_id = ? ORDER BY nome;", (nivelId,))
         rows = cur.fetchall()
         
         resultado = []
@@ -89,24 +89,24 @@ class AlunoDAO:
         return resultado
     
     def criarDeRow(self, row):
-        # Buscar a categoria usando o CategoriaDAO
-        nivelDao = NivelDAO(self.db)
+        # Buscar a nivel usando o NivelDAO
+        nivelDao = NivelDAO(self.__db)
         nivel = nivelDao.buscarPorId(row['nivel_id'])
         
         return Aluno(
-            aluno_id=row['id'],
+            id=row['id'],
             nome=row['nome'],
             contato=row['contato'],
-            nivel=Nivel,
-            tipoConducao=row['Condutor/Conduzido'],
-            ativo=bool(row['ativo'])
+            ativo=bool(row['ativo']),
+            tipoConducao=row['tipoConducao'],
+            nivel=nivel
         )
     
     def deletar(self, aluno: Aluno):        
-        cur = self.db.cursor()
-        cur.execute("DELETE FROM aluno WHERE id = ?;", (aluno.aluno_id,))
+        cur = self.__db.cursor()
+        cur.execute("DELETE FROM aluno WHERE id = ?;", (aluno.id,))
 
         return cur.rowcount > 0
     
-    def obterCategoria(self, aluno: Aluno):
+    def obterNivel(self, aluno: Aluno):
         return aluno.nivel
