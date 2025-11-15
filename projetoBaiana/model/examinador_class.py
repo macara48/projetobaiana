ce5168384890ba2c91feb06bd5b71aeda36efa09
Maria Clara Pires <80272737+macara48@users.noreply.github.com>
2025-11-15 15:09:42 -0300

    Update examinador_class.py
    Atualização de privacidade.

--- projetoBaiana/model/examinador_class.py
+++ projetoBaiana/model/examinador_class.py
@@ -1,8 +1,32 @@
-class Examinador():
-  def __init__(self, examinador_id: int, examinador: str, contato: str):
-    self.examinador_id = examinador_id
-    self.examinador = examinador
-    self.contato = contato
+class Examinador:
+  def __init__(self, id: int, nome: str, contato: str):
+    self.__id = id
+    self.__nome = nome
+    self.__contato = contato
+
+  @property
+  def id(self):
+    return self.__id
+  
+  @id.setter
+  def id(self, value):
+    self.__id = value
+  
+  @property
+  def nome(self):
+    return self.__nome
+  
+  @nome.setter
+  def nome(self, value):
+    self.__nome = value
+
+  @property
+  def contato(self):
+    return self.__contato
+  
+  @contato.setter
+  def contato(self, value):
+    self.__contato = value
 
   def __str__(self):
-    return f"{self.examinador_id} | {self.examinador} | {self.contato}"
+    return (f"Evento(id={self.__id}, nome='{self.__nome}', contato='{self.__contato}')")
