606b457f68a3e9ccb3979f3ccb088cd7e25f01dd
Maria Clara Pires <80272737+macara48@users.noreply.github.com>
2025-11-15 15:09:22 -0300

    Update evento_class.py
    Atualização de privacidade.

--- projetoBaiana/model/evento_class.py
+++ projetoBaiana/model/evento_class.py
@@ -1,9 +1,41 @@
-class Evento():
-  def __init__(self, evento_id: int, evento: str, dataEvento: str, homenageado: str):
-    self.evento_id = evento_id
-    self.evento = evento
-    self.dataEvento = dataEvento
-    self.homenageado = homenageado
+class Evento:
+  def __init__(self, id: int, nome: str, dataEvento: str, homenageado: str):
+    self.__id = id
+    self.__nome = nome
+    self.__dataEvento = dataEvento
+    self.__homenageado = homenageado
+
+  @property
+  def id(self):
+    return self.__id
+  
+  @id.setter
+  def id(self, value):
+    self.__id = value
+  
+  @property
+  def nome(self):
+    return self.__nome
+  
+  @nome.setter
+  def nome(self, value):
+    self.__nome = value
+  
+  @property
+  def dataEvento(self):
+    return self.__dataEvento
+  
+  @dataEvento.setter
+  def dataEvento(self, value):
+    self.__dataEvento = value
+  
+  @property
+  def homenageado(self):
+    return self.__homenageado
+  
+  @homenageado.setter
+  def homenageado(self, value):
+    self.__homenageado = value
 
   def __str__(self):
-    return f"{self.evento_id} | {self.evento} | {self.dataEvento} | {self.homenageado}"
+    return (f"Evento(id={self.__id}, nome='{self.__nome}', dataEvento='{self.__dataEvento}', homenageado='{self.__homenageado}')")
