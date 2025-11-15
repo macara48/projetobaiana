417b3bc5bdeb35804deb56e5d5921199a074a36d
Maria Clara Pires <80272737+macara48@users.noreply.github.com>
2025-11-15 15:10:37 -0300

    Update parametros_class.py
    Atualização de privacidade.

--- projetoBaiana/model/parametros_class.py
+++ projetoBaiana/model/parametros_class.py
@@ -1,13 +1,53 @@
 from model.nivel_class import Nivel
 from model.estiloDanca_class import EstiloDanca
 
-class Parametros():
-  def __init__(self, parametro_id: int, parametro: str, tipoConducao: str, estilo: EstiloDanca, nivel: Nivel):
-    self.parametro_id = parametro_id
-    self.parametro = parametro
-    self.tipoConducao = tipoConducao
-    self.estilo = estilo
-    self.nivel = nivel
+class Parametros:
+  def __init__(self, id: int, nome: str, tipoConducao: str, estilo: EstiloDanca, nivel: Nivel):
+    self.__id = id
+    self.__nome = nome
+    self.__tipoConducao = tipoConducao
+    self.__estilo = estilo
+    self.__nivel = nivel
+
+  @property
+  def id(self):
+    return self.__id
+  
+  @id.setter
+  def id(self, value):
+    self.__id = value
+  
+  @property
+  def nome(self):
+    return self.__nome
+  
+  @nome.setter
+  def nome(self, value):
+    self.__nome = value
+
+  @property
+  def tipoConducao(self):
+    return self.__tipoConducao
+  
+  @tipoConducao.setter
+  def tipoConducao(self, value):
+    self.__tipoConducao = value
+
+  @property
+  def estilo(self):
+    return self.__estilo
+  
+  @estilo.setter
+  def estilo(self, value):
+    self.__estilo = value
+
+  @property
+  def nivel(self):
+    return self.__nivel
+  
+  @nivel.setter
+  def nivel(self, value):
+    self.__nivel = value
 
   def __str__(self):
-    return f"{self.parametro_id} | {self.parametro} | {self.tipoConducao} | {self.estilo} | {self.nivel}"
+    return (f"Parametros(id={self.__id}, nome='{self.__nome}', tipoConducao='{self.__tipoConducao}', estilo='{self.__estilo}', nivel='{self.__nivel}')")
