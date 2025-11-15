ca6b57be8e9dd13513251be2f403ce8b07630c24
Maria Clara Pires <80272737+macara48@users.noreply.github.com>
2025-11-15 15:09:05 -0300

    Update estiloDanca_class.py
    Atualização de privacidade.

--- projetoBaiana/model/estiloDanca_class.py
+++ projetoBaiana/model/estiloDanca_class.py
@@ -1,7 +1,23 @@
-class EstiloDanca():
-  def __init__(self, estilo_id: int, estilo: str):
-    self.estilo_id = estilo_id
-    self.estilo = estilo
+class EstiloDanca:
+  def __init__(self, id: int, nome: str):
+    self.__id = id
+    self.__nome = nome
+
+  @property
+  def id(self):
+    return self.__id
+  
+  @id.setter
+  def id(self, value):
+    self.__id = value
+  
+  @property
+  def nome(self):
+    return self.__nome
+  
+  @nome.setter
+  def nome(self, value):
+    self.__nome = value
 
   def __str__(self):
-    return f"{self.estilo_id} | {self.estilo}"
+    return (f"Evento(id={self.__id}, nome='{self.__nome}')")
